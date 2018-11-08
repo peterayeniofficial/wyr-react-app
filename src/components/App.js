fccbb709be685325467e1e359b7c3ab25ad5be0b
peterayeniofficial <ayenicology@gmail.com>
2018-11-08 23:53:47 +0100

    implement basic routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container, Header } from "semantic-ui-react";
+import NavBar from "./NavBar";
+import Dashboard from "./Dashboard";
+import LeaderBoard from "./LeaderBoard";
+import AddQuestion from "./AddQuestion";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <p>Hello World</p>
-      </div>
+      <Router>
+        <Fragment>
+          <NavBar />
+          <Container>
+            <Route path="/" exact Component={Dashboard} />
+            <Route path="/add-question" Component={AddQuestion} />
+            <Route path="/leader-board" Component={LeaderBoard} />
+          </Container>
+        </Fragment>
+      </Router>
     );
   }
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Container, Menu } from "semantic-ui-react";
 
 class NavBar extends Component {
   render() {
     return (
       <div>
-        <p>Hello World</p>
+        <Menu fixed="top" inverted>
+          <Container>
+            <Menu.Item as="a" header>
+              Would You Rather?
+            </Menu.Item>
+            <Menu.Item as={Link} to="/">
+              Home
+            </Menu.Item>
+            <Menu.Item as={Link} to="/add-question">
+              New Question
+            </Menu.Item>
+            <Menu.Item as={Link} to="/leader-board">
+              Leader Board
+            </Menu.Item>
+          </Container>
+        </Menu>
       </div>
     );
   }
