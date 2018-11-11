9ff9cbc9fa606d48e8d1088b30b99277cc5d27f0
peterayeniofficial <ayenicology@gmail.com>
2018-11-11 19:55:25 +0100

    implement combine reducers

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -28,7 +28,7 @@ export default function users(state = {}, action) {
         ...state,
         [action.authedUser]: {
           ...state[action.authedUser],
-          questions: state[action.authedUser].questions.concat([action.id])
+          questions: state[action.authedUser].questions.concat([action.qid])
         }
       };
     default:
