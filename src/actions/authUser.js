d3280aeb59972a366d3100f564c4edb0598e0901
peterayeniofficial <ayenicology@gmail.com>
2018-11-11 15:26:12 +0100

    Add authedUser action creators

---
+++ src/actions/authUser.js
@@ -0,0 +1,16 @@
+export const SET_AUTH_USER = "SET_AUTH_USER";
+export const UN_SET_AUTH_USER = "UN_SET_AUTH_USER";
+
+export function setAuthUser(id) {
+  return {
+    type: SET_AUTH_USER,
+    id
+  };
+}
+
+export function unSetAuthUser(id) {
+  return {
+    type: UN_SET_AUTH_USER,
+    id
+  };
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Container, Header } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import Dashboard from "./Dashboard";
 import LeaderBoard from "./LeaderBoard";
@@ -10,14 +10,13 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Fragment>
+        <Container>
           <NavBar />
-          <Container>
-            <Route path="/" exact Component={Dashboard} />
-            <Route path="/add-question" Component={AddQuestion} />
-            <Route path="/leader-board" Component={LeaderBoard} />
-          </Container>
-        </Fragment>
+
+          <Route path="/" exact Component={Dashboard} />
+          <Route path="/add-question" Component={AddQuestion} />
+          <Route path="/leader-board" Component={LeaderBoard} />
+        </Container>
       </Router>
     );
   }
