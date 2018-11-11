20748752ad075a24c10ec8ed42eb4d9392e26bc1
peterayeniofficial <ayenicology@gmail.com>
2018-11-11 20:35:07 +0100

    Add logic to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,19 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { unSetAuthUser } from "../actions/authUser";
 import { Link } from "react-router-dom";
 import { Container, Menu, Image } from "semantic-ui-react";
 
 class NavBar extends Component {
+  logout = e => {
+    e.preventDefault();
+    const { history, dispatch } = this.props;
+    history.push("/");
+    dispatch(unSetAuthUser());
+  };
   render() {
+    const { authedUser } this.props
     return (
       <div>
         <Menu fixed="top" inverted>
@@ -11,7 +21,10 @@ class NavBar extends Component {
             <Menu.Item as="a" header>
               Would You Rather?
             </Menu.Item>
-            <Menu.Item as={Link} to="/">
+            {
+              authedUser ? 
+              <Fragment>
+              <Menu.Item as={Link} to="/">
               Home
             </Menu.Item>
             <Menu.Item as={Link} to="/add-question">
@@ -20,6 +33,10 @@ class NavBar extends Component {
             <Menu.Item as={Link} to="/leader-board">
               Leader Board
             </Menu.Item>
+              </Fragment>
+            : 
+              <Fragment>
+
             <Menu.Menu position="right">
               <Menu.Item>
                 <Image
@@ -30,8 +47,12 @@ class NavBar extends Component {
                 Hello, Peter
               </Menu.Item>
 
-              <Menu.Item name="Logout" />
+              <Menu.Item name="Logout" onClick={this.logout} />
             </Menu.Menu>
+              </Fragment>
+
+            }
+            
           </Container>
         </Menu>
       </div>
@@ -39,4 +60,10 @@ class NavBar extends Component {
   }
 }
 
-export default NavBar;
+function mapStateToProps({ authedUser }) {
+  return {
+    authedUser
+  };
+}
+
+export default withRouter(connect(mapStateToProps)(NavBar));
