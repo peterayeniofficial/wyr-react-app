0884edd30d70efe3f2da7cf8847c944371b788cf
peterayeniofficial <ayenicology@gmail.com>
2018-11-11 19:45:31 +0100

    add users reducers and update actions

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,60 @@
+import { saveQuestion, saveQuestionAnswer } from "../utils/api";
+import { showLoading, hideLoading } from "react-redux-loading";
+
 export const RECEIVE_USERS = "RECEIVE_USERS";
+export const ADD_USER_QUESTION = "ADD_USERS_QUESTION";
+export const ADD_USER_QUESTION_ANSWER = "ADD_USERS_QUESTION_ANSWER";
 
 export function receiveUsers(users) {
   return {
     type: RECEIVE_USERS,
     users
+  };
+}
+
+export function addUserQuestion(qid, authedUser) {
+  return {
+    type: ADD_USER_QUESTION,
+    qid,
+    authedUser
+  };
+}
+
+export function handleAddUserQuestion(optionOneText, optionTwoText) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    dispatch(showLoading());
+    return saveQuestion({
+      optionOneText,
+      optionTwoText,
+      author: authedUser
+    })
+      .then(info => dispatch(addUserQuestion(info)))
+      .then(() => dispatch(hideLoading()));
+  };
+}
+
+export function addUserQuestionAnswer(qid, authedUser, answer) {
+  return {
+    type: ADD_USER_QUESTION_ANSWER,
+    qid,
+    authedUser,
+    answer
+  };
+}
+
+export function handleAddUserQuestionAnswer(qid, answer) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    dispatch(showLoading());
+    return saveQuestionAnswer({
+      qid,
+      authedUser,
+      answer
+    })
+      .then(info => {
+        dispatch(addUserQuestionAnswer(info));
+      })
+      .then(() => dispatch(hideLoading));
   };
 }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,8 @@
-import { RECEIVE_USERS } from "../actions/users";
+import {
+  RECEIVE_USERS,
+  ADD_USER_QUESTION,
+  ADD_USER_QUESTION_ANSWER
+} from "../actions/users";
 
 export default function users(state = {}, action) {
   switch (action.type) {
@@ -6,6 +10,26 @@ export default function users(state = {}, action) {
       return {
         ...state,
         ...action.users
+      };
+    case ADD_USER_QUESTION_ANSWER:
+      const { authedUser, qid, answer } = action;
+      return {
+        ...state,
+        [authedUser]: {
+          ...state[authedUser],
+          answers: {
+            ...state[authedUser].answers,
+            [qid]: answer
+          }
+        }
+      };
+    case ADD_USER_QUESTION:
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...state[action.authedUser],
+          questions: state[action.authedUser].questions.concat([action.id])
+        }
       };
     default:
       return state;
