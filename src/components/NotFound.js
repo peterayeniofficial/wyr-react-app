d9b8b4a934aeab59c54875897fc2f6f52e495b73
peterayeniofficial <ayenicology@gmail.com>
2018-11-18 15:01:09 +0100

    Refactor NotFound to stateless component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,17 +1,15 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import { NavLink } from "react-router-dom";
 
-class NotFound extends Component {
-  render() {
-    return (
-      <Fragment>
-        <div style={{ marginTop: "60px" }}>
-          The page you are looking for is not Found
-        </div>
-        <NavLink to="/">Click here</NavLink> to go back to the home page
-      </Fragment>
-    );
-  }
-}
+const NotFound = () => {
+  return (
+    <Fragment>
+      <div style={{ marginTop: "60px" }}>
+        The page you are looking for is not Found
+      </div>
+      <NavLink to="/">Click here</NavLink> to go back to the home page
+    </Fragment>
+  );
+};
 
 export default NotFound;
